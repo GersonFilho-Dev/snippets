dadcb2d28cc405e9987d8426b1826f8fbbde8a42
Gerson C Filho <gersoncfilho@mac.com>
2024-05-04 09:20:12 +0100

    refact: refact home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import db from "@/db";
+
+export default async function Home() {
+  //TODO: implement the api call to fetch all the snippets
+
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippet) => {
+    return <div key={snippet.id}>{snippet.title}</div>;
+  });
+
+  return <div>{renderedSnippets}</div>;
 }
