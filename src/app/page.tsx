45cd41b23a57c66867842bfc6922ffb0cd0f2b45
Gerson C Filho <gersoncfilho@mac.com>
2024-05-06 08:43:53 +0100

    refact: add style to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,5 +9,14 @@ export default async function Home() {
     return <div key={snippet.id}>{snippet.title}</div>;
   });
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <main className="flex justify-center items-center bg-slate-100 w-3/4 h-3/4 mx-auto">
+      <div className="flex items-center justify-between">
+        <h1 className="mr-2">Snippets</h1>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Create
+        </button>
+      </div>
+    </main>
+  );
 }
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -26,36 +26,42 @@ export default function SnippetCreatePage() {
   }
 
   return (
-    <form action={createSnippet}>
-      <h3 className="font-bold m-3">Create a Snippet</h3>
-      <div className="flex flex-col gap-4">
-        <div className="flex gap-4">
-          <label className="w-12" htmlFor="title">
-            Title
-          </label>
-          <input
-            className="border rounded p-2 w-full"
-            type="text"
-            id="title"
-            name="title"
-          />
-        </div>
+    <div className="flex justify-center items-center h-screen">
+      <form
+        id="form"
+        action={createSnippet}
+        className="container max-w-[768px] flex flex-col"
+      >
+        <h3 className="font-bold m-3 text-center">Create a Snippet</h3>
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-4">
+            <label className="w-12" htmlFor="title">
+              Title
+            </label>
+            <input
+              className="border rounded p-2 w-full"
+              type="text"
+              id="title"
+              name="title"
+            />
+          </div>
 
-        <div className="flex gap-4">
-          <label className="w-12" htmlFor="code">
-            Code
-          </label>
-          <input
-            className="border rounded p-2 w-full"
-            type="text"
-            id="code"
-            name="code"
-          />
+          <div className="flex gap-4">
+            <label className="w-12" htmlFor="code">
+              Code
+            </label>
+            <input
+              className="border rounded p-2 w-full"
+              type="text"
+              id="code"
+              name="code"
+            />
+          </div>
+          <button className="bg-blue-500 text-white p-2 rounded" type="submit">
+            Create Snippet
+          </button>
         </div>
-        <button className="bg-blue-500 text-white p-2 rounded" type="submit">
-          Create Snippet
-        </button>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
